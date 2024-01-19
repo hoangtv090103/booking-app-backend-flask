5a96f1008cc47999c15691291d40d6c05651e691
hoangtv090103 <hoangtv090103@gmail.com>
2024-01-20 02:28:23 +0700

    connect to Railway Postgres

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from models import User, Account
+from database import db_session
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:f5GBG1F4C46-24eABgAAE2GAE*D4ECaf@' \
+                                        'viaduct.proxy.rlwy.net:34049/railway'
 
 
 @app.route('/')
@@ -10,12 +14,49 @@ def index():
 
 @app.route('/tutor-manager')
 def tutor_manager():
-    return {
-        'name': 'Flask API',
-        'dob': '1999-01-01',
-        'email': '123@gmail.com'
-    }
+    result = User.query.all()
+    if not result:
+        return jsonify({'message': 'No users found'})
+    result = [{
+        'id': user.id,
+        'name': user.name,
+        'email': user.email
+    } for user in result]
+    return jsonify(result)
+
+
+@app.route('/tutor-manager/login', methods=['GET', 'POST'])
+def login():
+    request_data = request.get_json()
+    username = request_data['username']
+    password = request_data['password']
+    result = Account.query.filter_by(login=username, password=password).first()
+    if result:
+        return jsonify(
+            {
+                'login': result.login,
+                # 'username': result.user.name,
+                # 'email': result.user.email
+            }
+        )
+    else:
+        return jsonify(
+            {
+                'message': 'Invalid username or password'
+            }
+        )
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 if __name__ == '__main__':
+    from models import User
+    from database import init_db
+    init_db()
+    acc = Account(login='admin', password='admin')
+    db_session.add(acc)
+    db_session.commit()
     app.run(port=5000, debug=True)
---
+++ models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Account(Base):
+    __tablename__ = 'account'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(50), unique=True)
+    username = Column(String(50), unique=True)
+    password = Column(String(120), unique=True)
+    user = relationship('User', backref='account', uselist=False)
+
+    def __init__(self, login=None, password=None):
+        self.login = login
+        self.password = password
+
+    def __repr__(self):
+        return f'<Account {self.username!r}>'
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    email = Column(String(120), unique=True)
+    account_id = Column(Integer, ForeignKey('account.id'), unique=True)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f'<User {self.name!r}>'
