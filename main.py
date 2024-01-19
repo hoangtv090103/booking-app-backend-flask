a1d17c02543bfc1bc24063cc9b11c4a4de08db37
hoangtv090103 <hoangtv090103@gmail.com>
2024-01-19 20:16:58 +0700

    Test API

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return "Hello World"
+
+
+@app.route('/tutor-manager')
+def tutor_manager():
+    return {
+        'name': 'Flask API',
+        'dob': '1999-01-01',
+        'email': '123@gmail.com'
+    }
+
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run(port=5000, debug=True)
